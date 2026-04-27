eb95802b8d7b3a00c5e377a8292571a2b2c6a3ac
tomoki-miso <107928941+tomoki-miso@users.noreply.github.com>
2026-04-27 17:34:00 +0900

    fix : コメントの充実と、最終フレームの計算ロジック修正

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -1,26 +1,25 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-
 # フレームのクラスを定義
 class Frame
   attr_accessor :result
   attr_reader :scores
-  
+
   def initialize(scores:)
     @scores = scores
     @result = nil
   end
-  
+
   def isStrike?
     scores.first == 'X'
   end
-  
+
   def isSpare?
-    frameScore == 10 && !isStrike?
+    frame_score == 10 && !isStrike?
   end
-  
-  def frameScore
+
+  def frame_score
     scores.map { |s| s == 'X' ? 10 : s.to_i }.sum
   end
 end
@@ -29,9 +28,7 @@ score = ARGV[0]
 scores = score.split(',')
 shots = []
 
-# score を shots に入れる
 scores.each do |s|
-  # 'X' の場合は、
   if s == 'X'
     shots << 'X'
     shots << 0
@@ -40,8 +37,10 @@ scores.each do |s|
   end
 end
 
-
+# ['X', 0]　はストライクの場合なので、X 単体で1フレームとする
 frames = shots.each_slice(2).to_a.map { |frame| frame == ['X', 0] ? ['X'] : frame }
+
+# 10フレーム目の追加投球が別フレームとして分割された場合、10フレーム目に結合する
 while frames.size > 10
   frames[9] = frames[9] + frames[10]
   frames.delete_at(10)
@@ -49,10 +48,13 @@ end
 
 frame_hash = {}
 
+# 各フレームを　hash, Frame にして扱いやすくする
 frames.size.times do |index|
   frame_hash[index] = Frame.new(scores: frames[index])
 end
 
+# 次と次の投球を得るために、次と次のフレームの中身を取り出す
+# 次と次の投球なのは、ストライクが2回続いたときのために多めに取っている
 def next_rolls(frame_hash, start_index)
   tmp =
     frame_hash[start_index + 1]&.scores.to_a +
@@ -63,18 +65,21 @@ end
 
 total = 0
 
+# フレームごとに得点計算
 frame_hash.each do |index, frame|
   nexts = next_rolls(frame_hash, index)
 
   frame.result =
-    if frame.isStrike?
-      frame.frameScore + nexts[0].to_i + nexts[1].to_i
+    if index == 9
+      frame.frame_score
+    elsif frame.isStrike?
+      frame.frame_score + nexts[0].to_i + nexts[1].to_i
 
     elsif frame.isSpare?
-      frame.frameScore + nexts[0].to_i
+      frame.frame_score + nexts[0].to_i
 
     else
-      frame.frameScore
+      frame.frame_score
     end
 
   total += frame.result
