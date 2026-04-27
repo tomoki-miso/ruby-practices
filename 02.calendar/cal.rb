1f716f918e98a527a4b8f58b24b789617c65733a
tomoki-miso <107928941+tomoki-miso@users.noreply.github.com>
2026-04-27 10:45:57 +0900

    fix lint への対応

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -1,16 +1,16 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-require "optparse"
-require "date"
+require 'optparse'
+require 'date'
 opt = OptionParser.new
 
 week_names = %w[日 月 火 水 木 金 土]
 params = {}
 
 # 入力
-opt.on("-y VAL", Integer) { |v| params[:y] = v }
-opt.on("-m VAL", Integer) { |v| params[:m] = v }
+opt.on('-y VAL', Integer) { |v| params[:y] = v }
+opt.on('-m VAL', Integer) { |v| params[:m] = v }
 
 begin
   opt.parse!(ARGV)
@@ -25,7 +25,7 @@ rescue OptionParser::ParseError => e
   warn "正しいオプションを入れてね\n#{e}"
   exit 1
 rescue Date::Error
-  warn "正しい年月を入れてね（例: -y 2026 -m 1)"
+  warn '正しい年月を入れてね（例: -y 2026 -m 1)'
   exit 1
 end
 
@@ -38,7 +38,7 @@ puts
 
 days.each_slice(7) do |week|
   week.each do |day|
-    print day.nil? ? " ".center(3) : day.day.to_s.center(3)
+    print day.nil? ? ' '.center(3) : day.day.to_s.center(3)
   end
   puts
 end
