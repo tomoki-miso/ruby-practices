9ee44fb0993f7fca0cbbe626299ff2aea38964f3
tomoki-miso <107928941+tomoki-miso@users.noreply.github.com>
2026-05-07 12:30:33 +0900

    refactor : ストライクとスペアの判定ロジックを修正,フレームスコアの計算を簡素化

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -11,67 +11,64 @@ class Frame
   end
 
   def strike?
-    scores.first == 'X'
+    scores.first == 10
   end
 
   def spare?
-    frame_score == 10 && !strike?
+    scores.size == 2 && scores[0] + scores[1] == 10 && !strike?
   end
 
   def frame_score
-    scores.map { |s| roll_score(s) }.sum
-  end
-
-  def roll_score(roll)
-    roll == 'X' ? 10 : roll.to_i
+    scores.sum
   end
 end
 
-
 def next_rolls(frames, start_index)
-  frames[(start_index + 1)..]&.flatten || []
+  frames.drop(start_index + 1).flat_map(&:scores)
 end
 
 score = ARGV[0]
-scores = score.split(',')
+scores = score.split(",")
 shots = []
 
 scores.each do |s|
-  if s == 'X'
-    shots << 'X'
-    shots << 0
+  shots << if s == "X"
+    10
   else
-    shots << s
+    s.to_i
   end
 end
 
-# ['X', 0]　はストライクの場合なので、X 単体で1フレームとする
-frames = shots.each_slice(2).to_a.map { |frame| frame == ['X', 0] ? ['X'] : frame }
+frames = []
 
-# 10フレーム目の追加投球が別フレームとして分割された場合、10フレーム目に結合する
-while frames.size > 10
-  frames[9] = frames[9] + frames[10]
-  frames.delete_at(10)
+shot_index = 0
+9.times do
+  if shots[shot_index] == 10
+    frames << Frame.new(scores: [10])
+    shot_index += 1
+  else
+    frames << Frame.new(scores: shots[shot_index, 2])
+    shot_index += 2
+  end
 end
 
-total = 0
+# 10フレーム目は残り全部
+frames << Frame.new(scores: shots[shot_index..])
 
-frames.each_with_index do |frame_scores, index|
-  frame = Frame.new(scores: frame_scores)
+total = frames.each_with_index.sum do |frame, index|
   nexts = next_rolls(frames, index)
+  each_frame_score = frame.frame_score
 
   frame.result =
     if index == 9
-      frame.frame_score
+      each_frame_score
     elsif frame.strike?
-      frame.frame_score + frame.roll_score(nexts[0]) + frame.roll_score(nexts[1])
+      each_frame_score + nexts[0] + nexts[1]
     elsif frame.spare?
-      frame.frame_score + frame.roll_score(nexts[0])
+      each_frame_score + nexts[0]
     else
-      frame.frame_score
+      each_frame_score
     end
-
-  total += frame.result
 end
 
 puts total
