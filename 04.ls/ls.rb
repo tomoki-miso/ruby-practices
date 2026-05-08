d1d3600ddfa96d7e2c4fec372ed7205faef7bf8a
tomoki-miso <107928941+tomoki-miso@users.noreply.github.com>
2026-05-08 14:42:09 +0900

    refactor : print_formatted_entries メソッドの整形処理を改善

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -4,12 +4,21 @@ COLUMN_SIZE = 3
 
 def print_formatted_entries(entries)
   row_size = (entries.size.to_f / COLUMN_SIZE).ceil
-
   chunks = entries.each_slice(row_size).to_a
 
+  column_widths = chunks.map do |chunk|
+    chunk.map(&:length).max || 0
+  end
+
   row_size.times do |row_index|
-    line = chunks.map { |chunk| chunk[row_index] }
-    puts line.join(" ")
+    line = chunks.each_with_index.map do |chunk, column_index|
+      item = chunk[row_index]
+      next nil unless item
+
+      item.ljust(column_widths[column_index])
+    end
+
+    puts line.compact.join("  ")
   end
 end
 
