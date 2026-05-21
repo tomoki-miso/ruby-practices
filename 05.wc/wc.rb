3a140e556d816f67f6e0cc46c40246e7da6cc07b
tomoki-miso <107928941+tomoki-miso@users.noreply.github.com>
2026-05-21 18:58:14 +0900

    refactor: output_countsメソッドの処理を整理し、カウントの計算を簡素化

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -27,18 +27,27 @@ end
 
 def output_counts(options, input, path = nil)
   no_options = options.empty?
+  converted_options = no_options ? { line: true, word: true, byte: true } : options
+  output = []
 
-  line_count = input.lines.size if no_options || options[:line]
-  word_count = input.split.size if no_options || options[:word]
-  byte_count = input.bytesize if no_options || options[:byte]
+  output << calculate_counts(input)[:line] if converted_options[:line]
+  output << calculate_counts(input)[:word] if converted_options[:word]
+  output << calculate_counts(input)[:byte] if converted_options[:byte]
 
-  output = [line_count, word_count, byte_count]
-  
   output.each do |count|
-    print "#{count.to_s.rjust(8)}"
+    print count.to_s.rjust(8)
   end
+
   print " #{path}" if path
   puts ''
+end
+
+def calculate_counts(input)
+  {
+    line: input.lines.size,
+    word: input.split.size,
+    byte: input.bytesize
+  }
 end
 
 main
